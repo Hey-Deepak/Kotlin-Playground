532f69652c23bd6d55ffcd7cdf207e27d4bd660d
Deepak Choudhary <choudharydeepak990@gmail.com>
2025-04-03 23:31:17 +0530

    Patter3 Added
    5
    1
    12
    123
    1234
    12345

---
+++ src/Practice/pattern/P3.kt
@@ -0,0 +1,12 @@
+package org.example.Practice.pattern
+
+fun main() {
+    val line = readln().toInt()
+
+    for (i in 1..line){
+        for (j in 1..i){
+            print(j)
+        }
+        println()
+    }
+}
